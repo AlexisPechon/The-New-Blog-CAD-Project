3355dbdc1a4b858b0dddb2dd665c2f81ed5d7d94
Alexis Pechon <arpechon@gmail.com>
2023-07-10 21:08:25 +0100

    6thCommit

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -4,4 +4,15 @@ class CommentsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  test "to test if the body of the comments has a valid input" do
+    blogBody = Article.new(body: "This is an amazing example of a body")
+    assert_not blogBody.valid?
+  end
+
+  test "to test if the body of the comments has an invalid input" do
+    blogBody = Article.new(body: nil)
+    assert_not blogBody.valid?
+  end
+  
 end
