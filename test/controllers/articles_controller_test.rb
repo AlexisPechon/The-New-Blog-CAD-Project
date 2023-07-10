2dcd29a99e0ea40ad544af5371d17d57bd3be9e3
Alexis Pechon <arpechon@gmail.com>
2023-07-10 21:01:39 +0100

    5thCommit

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -5,7 +5,14 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
   #   assert true
   # end
 
-  test "if the title is a valid" do
+  test "if the title has a valid input" do
     blogTitle = Article.new(title: "The Most Basic Game")
     assert_not blogTitle.valid?
+  end
+
+  test "if the title has an invalid input" do
+    blogTitle = Article.new(title: nil)
+    assert_not blogTitle.valid?
+  end
+  
 end
