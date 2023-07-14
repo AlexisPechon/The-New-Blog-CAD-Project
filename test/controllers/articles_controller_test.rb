b726b0408428575335e980f67692a5bc34a019c5
Alexis Pechon <arpechon@gmail.com>
2023-07-14 20:17:52 +0100

    7thCommit

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -5,6 +5,7 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
   #   assert true
   # end
 
+  # Unit Testing
   test "if the title has a valid input" do
     blogTitle = Article.new(title: "The Most Basic Game")
     assert_not blogTitle.valid?
@@ -14,5 +15,28 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
     blogTitle = Article.new(title: nil)
     assert_not blogTitle.valid?
   end
+  # End of Unit Testing
+
+  # Functional Testing
+  test "if it works" do
+    get "http://127.0.0.1:3000/articles"
+    assert_response:success
+  end
+
+  test "if login can be accessed" do
+    get "http://127.0.0.1:3000/login"
+    assert_response:success
+  end
+  
+  #End of Functional Testing
+
+  # Integration Testing
+
+  test "should get the homepage" do
+    get '/articles'
+    assert_response:success
+  end
+
+  # End of Integration Testing
   
 end
