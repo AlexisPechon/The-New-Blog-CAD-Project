92c8a316d12979ebae7ea736790934ae3bfc528a
Alexis Pechon <arpechon@gmail.com>
2023-07-16 22:57:48 +0100

    14thCommit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,20 @@
 class SessionsController < ApplicationController
-    def new
-    end
-  
+    # def new
+    # end
+
     def create
-        puts params.inspect
-      user = User.find_by(email: params[:session][:email].downcase)
-      if user && user.authenticate(params[:session][:password])
-        # Log the user in and redirect to the user's profile page
-        log_in user
-        redirect_to user
+      # @user = User.find_by(email: params[:email]="arpechon@outlook.com")
+      @user = User.find_by(email: "arpechon@outlook.com")
+      # if @user && @user.authenticate(params[:password]="aaaa")
+      if @user && @user.authenticate("1234")
+        # Log the user in and redirect to the root URL.
+        #log_in user
+        redirect_to root_url
       else
-        # Display an error message
-        flash.now[:danger] = 'You have entered an invalid email or password.'
-        render 'new'
+        # Displays an Error Message when something goes wrong during the login process.
+        errorMessage = 'Something went terribly wrong. We apologise for the inconvenience...'
+        redirect_to login_path, notice: errorMessage 
+        #render 'new'
       end
     end
   
