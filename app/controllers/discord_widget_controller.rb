86b35c346e702c416e6612dd168d4884d1c9bc31
Alexis Pechon <arpechon@gmail.com>
2023-07-25 20:15:23 +0100

    29thCommit

--- app/controllers/discord_widget_controller.rb
+++ app/controllers/discord_widget_controller.rb
@@ -1,8 +1,10 @@
 class DiscordWidgetController < ApplicationController
-    def fetch_data
-        url = 'https://discord.com/api/guilds/1130257966415745124/widget.json'
-        uri = URI(url)
     
+    def fetch_data
+
+        url = 'https://discord.com/api/guilds/973969434127507526/widget.json'
+        uri = URI(url)
+
         response = Net::HTTP.get(uri)
         @widget_data = JSON.parse(response)
       end
