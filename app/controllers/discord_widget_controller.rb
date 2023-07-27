89877b94866487ad57d299e5b64b54524633b98d
Alexis Pechon <arpechon@gmail.com>
2023-07-27 01:38:10 +0100

    32ndCommit

--- app/controllers/discord_widget_controller.rb
+++ app/controllers/discord_widget_controller.rb
@@ -1,11 +1,12 @@
 class DiscordWidgetController < ApplicationController
     
     def fetch_data
+      #This method controls the logic that is require to read the API call
+      #and to display the contents of the API call onto the webpage
+        url = 'https://discord.com/api/guilds/973969434127507526/widget.json' #Variables stores the URL
+        uri = URI(url) #Parses the information as a URI
 
-        url = 'https://discord.com/api/guilds/973969434127507526/widget.json'
-        uri = URI(url)
-
-        response = Net::HTTP.get(uri)
-        @widget_data = JSON.parse(response)
+        response = Net::HTTP.get(uri) #Gets the URI link
+        @widget_data = JSON.parse(response) #Parses the JSON of the URI Link so it could be understood by the program
       end
 end
--- app/controllers/joke_controller.rb
+++ app/controllers/joke_controller.rb
@@ -1,7 +1,9 @@
 require 'httparty'
 class JokeController < ApplicationController
   def joke
-    response = HTTParty.get('https://v2.jokeapi.dev/joke/Programming?blacklistFlags=racist,sexist,explicit')
+    #This method controls and handles the logic necessary to parse and read the API calls
+    #This will then be used inside of the file joke.html.erb so it can be displayed on the webpage
+    response = HTTParty.get('https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=twopart')
     @random = JSON.parse(response.body)
     @randoms = @random['setup']
     @randomd = @random['delivery']
