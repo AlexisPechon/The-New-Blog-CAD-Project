0c9d3a6f40423d7820b40520fbfa47d8eeaee861
Alexis Pechon <arpechon@gmail.com>
2023-07-29 18:30:51 +0100

    41stCommit

--- app/controllers/discord_widget_controller.rb
+++ app/controllers/discord_widget_controller.rb
@@ -1,12 +1,10 @@
+require 'httparty'
 class DiscordWidgetController < ApplicationController
     
     def fetch_data
       #This method controls the logic that is require to read the API call
       #and to display the contents of the API call onto the webpage
-        url = 'https://discord.com/api/guilds/973969434127507526/widget.json' #Variables stores the URL
-        uri = URI(url) #Parses the information as a URI
-
-        response = Net::HTTP.get(uri) #Gets the URI link
-        @widget_data = JSON.parse(response) #Parses the JSON of the URI Link so it could be understood by the program
+        response = HTTParty.get('https://discord.com/api/guilds/973969434127507526/widget.json') #Variable store the get request of the API Call
+        @widget_data = JSON.parse(response.body) #Parses the JSON data so it could be understood by Ruby.
       end
 end
